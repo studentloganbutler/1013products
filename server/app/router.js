48dadc1af10b3579f7cd4ac342a8b68733923698
Logan Butler <logan.butler@student.swic.edu>
2021-10-20 20:24:36 -0500

    Add router for product/:id

--- server/app/router.js
+++ server/app/router.js
@@ -1,6 +1,7 @@
 import Router from "express";
 import config from "./config.js";
 import client from "./db/connections/client.js";
+import { ObjectId } from "mongoDB";
 
 const {
   db: { name, collectionName },
@@ -18,7 +19,15 @@ router.get("/products", (_, res) => {
     .collection("products")
     .find()
     .toArray();
-  res.send(products);
+  res.json(products);
+});
+
+router.get("/products/:id", async (req, res) => {
+  const product = await client
+    .db(name)
+    .collection(collectionName)
+    .findOne({ _id: ObjectId(req.params.id) });
+  res.json(product);
 });
 
 export default router;
