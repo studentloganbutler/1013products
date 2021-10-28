a42c234cb646c5179be858d535970eec6a14f88f
Logan Butler <logan.butler@student.swic.edu>
2021-10-27 20:19:09 -0500

    Correct config import

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,4 +1,4 @@
-import { config } from "dotenv";
+import { config } from "../../config.js";
 import once from "./connections/once.js";
 import productsData from "./data.js";
 
@@ -6,10 +6,12 @@ const {
   db: { name, collectionName },
 } = config;
 
+console.log(name, collectionName);
+
 // Anonymous IIFE - Immediately Invoked Function Expression
 (async () => {
   const connections = await once.connect();
-  await connections.db(name).collection(name).deleteMany({});
-  await connections.db(name).collection(name).insertMany(productsData);
+  await connections.db(name).collection(collectionName).deleteMany({});
+  await connections.db(name).collection(collectionName).insertMany(productsData);
   connections.close();
 })();
